4407fb30512ed9beff3d9591bb64075586600ae3
Sean Slavin <sean.slavin@vidrovr.com>
2023-10-27 19:51:06 +0000

    Documentation

--- docs/source/projects.rst
+++ docs/source/projects.rst
@@ -4,4 +4,13 @@ Projects
 Creating, deleting, reading and updating project data
 -----------------------------------------------------
 
-Lorem ipsom blah blah blah
+To retrieve information about projects or to create, update or delete a project, 
+you can use the vidrovr.resources.projects.Project object.
+
+.. autofunction:: vidrovr.resources.projects.Project.create
+
+.. autofunction:: vidrovr.resources.projects.Project.delete
+
+.. autofunction:: vidrovr.resources.projects.Project.read
+
+.. autofunction:: vidrovr.resources.projects.Project.update
--- vidrovr/resources/projects/projects.py
+++ vidrovr/resources/projects/projects.py
@@ -19,6 +19,14 @@ class Project(BaseModel):
 
     @classmethod
     def read(cls, project_id: str = None):
+        """
+        Retrieve a list of projects in the organization or details about a specific project.
+        
+        :param project_id: ID of the projec or None
+        :type project_id: str
+        :return: Array of project infomration or a single project
+        :rtype: list[ProjectData] or ProjectData
+        """
         if project_id is None:
             url = f'projects/'
         else:
@@ -40,6 +48,14 @@ class Project(BaseModel):
     
     @classmethod
     def delete(cls, project_id: str):
+        """
+        Delete a specific project from the organization.
+        
+        :param project_id: ID of the project
+        :type project_id: str
+        :return: JSON string of the HTTP response
+        :rtype: str
+        """
         url      = f'projects/{project_id}'
         response = Client.delete(url)
 
@@ -47,12 +63,22 @@ class Project(BaseModel):
     
     @classmethod
     def update(cls, project_id: str, user_id: str, name: str, operation: str):
+        """
+        Update a project in an organization.
+        
+        :param project_id: ID of the project
+        :type project_id: str
+        :param name: Name of the project
+        :type name: str
+        :return: JSON string of the HTTP response
+        :rtype: str
+        """
         url      = f'projects/{project_id}'
         payload  = {
             'data': {
                 'name': name,
-                'operation': operation, # 'add' or 'remove'
-                'user_ids': [user_id]
+                #'operation': operation, # 'add' or 'remove'
+                #'user_ids': [user_id]
             }
         }
         response = Client.patch(url, payload)
@@ -61,6 +87,16 @@ class Project(BaseModel):
     
     @classmethod
     def create(cls, user_id: str, name: str):
+        """
+        Create a new project in the organization.
+        
+        :param user_id: ID of the user associated with the project
+        :type user_id: str
+        :param name: Name of the project
+        :type name: str
+        :return: JSON string of the HTTP response
+        :rtype: str
+        """
         url      = f'projects/'
         payload  = {
             'data': {
