24f4bb07de92d3765165a660ba768726da14ef5c
Sean Slavin <sean.slavin@vidrovr.com>
2023-12-05 20:02:26 +0000

    Removal of absolute paths and update method returns

--- vidrovr/resources/search/__init__.py
+++ vidrovr/resources/search/__init__.py
@@ -1,2 +1,2 @@
-from .search import Search, SavedSearch, SearchFeedItems, SearchFacet, SearchQuery, SearchData
-from .search_appearances import SearchAppearances, SearchAppearancesData, AppearancesFeedItems, AppearancesFacet, AppearancesFeedId, AppearancesFacetWithFacets
+from vidrovr.resources.search.search import Search, SavedSearch, SearchFeedItems, SearchFacet, SearchQuery, SearchData
+from vidrovr.resources.search.search_appearances import SearchAppearances, SearchAppearancesData, AppearancesFeedItems, AppearancesFacet, AppearancesFeedId, AppearancesFacetWithFacets
--- vidrovr/resources/search/search.py
+++ vidrovr/resources/search/search.py
@@ -1,87 +1,262 @@
 import json
 
-from ...core import Client
+from vidrovr.core import Client
 
-from dataclasses import dataclass
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator
+from icecream import ic
 
-@dataclass
-class SearchFeedItems:
-    type: str
-    field: str
+class SearchFeedItems(BaseModel):
+    type: str = None
+    field: str = None
 
-@dataclass
-class SearchFacet:
+    @validator("type", pre=True)
+    def check_type(cls, value):
+        if value is None:
+            value = 'Default'
+
+        return value
+
+    @validator("field", pre=True)
+    def check_field(cls, value):
+        if value is None:
+            value = 'Default'
+
+        return value
+
+class SearchFacet(BaseModel):
     feed_items: SearchFeedItems
 
-@dataclass
-class SearchQuery:
-    project_id: str
-    query: str
-    sort: str
-    limit: int
-    offset: int
+class SearchQuery(BaseModel):
+    project_id: str = None
+    query: str = None
+    sort: str = None
+    limit: int = 0
+    offset: int = 0
     facet: SearchFacet
 
-@dataclass
-class SearchData:
-    name: str
+    @validator("query", pre=True)
+    def check_query(cls, value):
+        if value is None:
+            value = 'Default'
+
+        return value
+
+    @validator("sort", pre=True)
+    def check_sort(cls, value):
+        if value is None:
+            value = 'Default'
+
+        return value
+
+class SearchData(BaseModel):
+    name: str = None
     query: SearchQuery
     collection: str = 'appearances'
 
-@dataclass
-class SavedSearch:
+class SavedSearch(BaseModel):
+    search_id: str = None
     data: SearchData
 
-class Search(BaseModel):
+    @validator("search_id", pre=True)
+    def check_search_id(cls, value):
+        if value is None:
+            value = 'Default'
+
+        return value
+
+class Search:
 
     @classmethod
-    def read(cls, project_id: str, search_id: str):
+    def read(cls, project_id: str, search_id: str = None):
+        """
+        Retrieve the results of a saved search
+
+        :param project_id: ID of the project containing the saved search
+        :type project_id: str
+        :param search_id: ID of the saved search
+        :type search_id: str
+        :return: A list of saved searches
+        :rtype: list[SavedSearch]
+        """
         if search_id is None:
             url = f'search/saved/{project_id}'
         else:
             url = f'search/saved/{project_id}/{search_id}'
 
         response = Client.get(url)
+        searches = []
 
-        return response
+        if response is not None:
+            for item in response:
+                try:
+                    searchFeedItems = SearchFeedItems(
+                        type=None,
+                        field=None
+                    )
+                    searchFacet = SearchFacet(
+                        feed_items=searchFeedItems
+                    )
+                    searchQuery = SearchQuery(
+                        project_id=project_id,
+                        query=None,
+                        sort=None,
+                        limit=0,
+                        offset=0,
+                        facet=searchFacet
+                    )
+                    searchData = SearchData(
+                        name=item['name'],
+                        query=searchQuery,
+                        collection='appearances'
+                    )
+                    savedSearch = SavedSearch(
+                        search_id=item['id'],
+                        data=searchData
+                    )
+
+                    searches.append(savedSearch)
+
+                except ValidationError as e:
+                    print(f'Search.read(): Validation error for {item}: {e}')
+
+            return searches
+        else:
+            return response
 
     @classmethod
     def delete(cls, project_id: str, search_id: str):
+        """
+        Delete a saved search
+
+        :param project_id: ID of the project containing the saved search
+        :type project_id: str
+        :param search_id: ID of the saved search to delete
+        :type search_id: str
+        """
+        ic('delete')
         url      = f'search/saved/{project_id}/{search_id}'
         response = Client.delete(url)
-
+        
         return response
 
     @classmethod
     def update(cls, project_id: str, search_id: str, data: SavedSearch):
-        url          = f'search/saved/{project_id}/{search_id}'
-        payload_json = json.dumps(data, default=lambda o: o.__dict__)
+        """
+        Update a saved search
 
-        # when updating a saved search, we don't need the collection attribute
+        :param project_id: ID of the project containing the saved search
+        :type project_id: str
+        :param search_id: ID of the saved search to update
+        :type search_id: str
+        :param data: Data model to use to update the saved search
+        :type data: SavedSearch
+        """
+        url = f'search/saved/{project_id}/{search_id}'
+        data_json = data.model_dump_json()
+
+        # when updating a saved search, we don't need the collection key
         # so strip it out before sending the request
-        payload  = cls._exclude_collection(payload_json)
+        payload = cls._exclude_collection(data_json, 'collection')
         response = Client.patch(url, payload)
 
-        return response
+        if response is not None:
+            searchFeedItems = SearchFeedItems(
+                type=None,
+                field=None
+            )
+            searchFacet = SearchFacet(
+                feed_items=searchFeedItems
+            )
+            searchQuery = SearchQuery(
+                project_id=project_id,
+                query=None,
+                sort=None,
+                limit=0,
+                offset=0,
+                facet=searchFacet
+            )
+            searchData = SearchData(
+                name=response['name'],
+                query=searchQuery,
+                collection='appearances'
+            )
+            savedSearch = SavedSearch(
+                search_id=response['id'],
+                data=searchData
+            )
+
+            return savedSearch
+        else:
+            return response
 
     @classmethod
     def create(cls, project_id: str, data: SavedSearch):
-        url      = f'search/saved/{project_id}'
-        payload  = json.dumps(data, default=lambda o: o.__dict__)
+        """
+        Create a saved search
+
+        :param project_id: ID of the project to contain the saved search
+        :type project_id: str
+        :param data: Data model to use for creating the saved search
+        :type data: SavedSearch
+        :return: A saved search data model on success
+        :rtype: SavedSearch
+        """
+        url = f'search/saved/{project_id}'
+        data_json = data.model_dump_json()
+
+        # when creating a saved search, we don't need the search_id key
+        # so strip it out before sending the request
+        payload = cls._exclude_collection(data_json, 'search_id')
         response = Client.post(url, payload)
 
-        return response
+        if response is not None:
+            searchFeedItems = SearchFeedItems(
+                type=None,
+                field=None
+            )
+            searchFacet = SearchFacet(
+                feed_items=searchFeedItems
+            )
+            searchQuery = SearchQuery(
+                project_id=project_id,
+                query=None,
+                sort=None,
+                limit=0,
+                offset=0,
+                facet=searchFacet
+            )
+            searchData = SearchData(
+                name=response['name'],
+                query=searchQuery,
+                collection='appearances'
+            )
+            savedSearch = SavedSearch(
+                search_id=response['id'],
+                data=searchData
+            )
+
+            return savedSearch
+        else:
+            return response
     
-    def _exclude_collection(cls, data):
+    def _exclude_collection(data, param: str):
         data_dict = json.loads(data)
-        value     = data_dict['data']['collection'] if 'collection' in data_dict.get('data', {}) else None
+        
+        # remove the collection key if we're updating the search
+        if param == 'collection':
+            value = data_dict['data']['collection'] if 'collection' in data_dict.get('data', {}) else None
 
-        # make sure we got a value from the collection key
-        if value is not None:
-            if 'data' in data_dict and 'collection' in data_dict['data']:
-                data_dict['data'].pop('collection')
+            # make sure we got a value from the collection key
+            if value is not None:
+                if 'data' in data_dict and 'collection' in data_dict['data']:
+                    data_dict['data'].pop('collection')
+        # remove the search_id key if we're creating
+        elif param == 'search_id':
+            value = data_dict['search_id'] if 'search_id' in data_dict else None
 
-        modified_payload = json.dumps(data_dict)
+            if value is not None:
+                ic(value)
+                if 'search_id' in data_dict:
+                    del data_dict['search_id']
 
-        return json.dumps(modified_payload)
+        return data_dict
