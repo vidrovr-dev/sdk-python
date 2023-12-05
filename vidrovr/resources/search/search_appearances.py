46129e2b214dc54d8b5af2e96ebd18e355c9353a
Sean Slavin <sean.slavin@vidrovr.com>
2023-12-05 21:55:14 +0000

    Removal of absolute paths and update method returns

--- vidrovr/resources/search/search_appearances.py
+++ vidrovr/resources/search/search_appearances.py
@@ -1,72 +1,53 @@
 import json
 
-from ...core import Client
+from vidrovr.core import Client
 
-from dataclasses import dataclass
 from pydantic import BaseModel
 from typing import List
 
-@dataclass
-class AppearancesFeedItems:
-    type: str
-    field: str
-    limit: int
+class AppearancesFeedItems(BaseModel):
+    type: str = None
+    field: str = None
+    limit: int = 0
 
-@dataclass
-class AppearancesFacet:
+class AppearancesFacet(BaseModel):
     feed_items: AppearancesFeedItems
 
-@dataclass
-class AppearancesFeedId:
-    type: str
-    field: str
-    limit: int
+class AppearancesFeedId(BaseModel):
+    type: str = None
+    field: str = None
+    limit: int = 0
     facet: AppearancesFacet
 
-@dataclass
-class AppearancesFacetWithFacets:
+class AppearancesFacetWithFacets(BaseModel):
     feed_id: AppearancesFeedId
 
-@dataclass
-class SearchAppearancesData:
-    project_id: str
-    query: str
-    filter: List[str]
-    sort: str
-    limit: int
-    offset: int
+class SearchAppearancesData(BaseModel):
+    project_id: str = None
+    query: str = None
+    filter: List[str] = None
+    sort: str = None
+    limit: int = 0
+    offset: int = 0
     facet: AppearancesFacetWithFacets
 
-class SearchAppearances(BaseModel):
+class SearchAppearances:
 
     @classmethod
     def create(cls, project_id: str, data: SearchAppearancesData):
-        url     = 'search/appearances'
-        payload = json.dumps(data, default=lambda o: o.__dict__)
-        '''payload = {
-            'project_id': project_id, 
-            'query': '*',
-            'filter': [
-                '{!join fromIndex=appearances method=crossCollection from=feed_item_id to=feed_item_id df=_text_}dog'
-            ],
-            'sort': 'original_time desc',
-            'limit': 10,
-            'offset': 0,
-            'facet': {
-                'feed_id': {
-                    'type': 'terms',
-                    'field': 'feed_id',
-                    'limit': -1,
-                    'facet': {
-                        'feed_items': {
-                            'type': 'terms',
-                            'field': 'feed_item_id',
-                            'limit': -1
-                        }
-                    }
-                }
-            }
-        }'''
+        """
+        Create an appearances search
+
+        :param project_id: ID of the project containing the search
+        :type project_id: str
+        :param data: Object model containing the details of the search
+        :type data: SearchAppearancesData
+        """
+        url = 'search/appearances'
+        payload = data.model_dump_json()
         response = Client.post(url, payload)
 
+        #if response is not None:
+            # do stuff
+        #else:
         return response
